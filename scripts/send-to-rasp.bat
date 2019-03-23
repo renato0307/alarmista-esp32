621b25a18725ffcaded65924570618d8b065ae83
Renato Torres <renato.torres@unit-consulting.ae>
2019-03-23 18:06:46 +0000

    Added temperature read from DHT

--- scripts/send-to-rasp.bat
+++ scripts/send-to-rasp.bat
@@ -1,2 +1,3 @@
 @echo off
 scp src\* pi@rasp2esp32.ddns.net:/home/pi/workspace/alarmista-esp32/src
+scp *.ini pi@rasp2esp32.ddns.net:/home/pi/workspace/alarmista-esp32
--- src/SunriseState.cpp
+++ src/SunriseState.cpp
@@ -1,10 +1,13 @@
-
 #include <Arduino.h>
 #include <ArduinoLog.h>
+#include <DHTesp.h>
 
 #include "GlobalStatus.h"
 
 #define BUTTON_INTERRUPT_PIN 13
+#define DHT_PIN 4
+
+DHTesp dht;
 
 /* ========================================================================= 
    Private functions 
@@ -15,6 +18,67 @@ void buttonInterrupt()
 {
   globalStatus.goToConfig = true;
 }
+
+// logTemperature reads the temperature from DHT and prints it to log
+bool logTemperature() {
+  // Reading temperature for humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  TempAndHumidity newValues = dht.getTempAndHumidity();
+  // Check if any reads failed and exit early (to try again).
+  if (dht.getStatus() != 0) {
+    Log.error("DHT11 error status: %s\n", dht.getStatusString());
+    return false;
+  }
+
+  ComfortState cf;
+
+  float heatIndex = dht.computeHeatIndex(newValues.temperature, newValues.humidity);
+  float dewPoint = dht.computeDewPoint(newValues.temperature, newValues.humidity);
+  //float cr = dht.getComfortRatio(cf, newValues.temperature, newValues.humidity);
+
+  String comfortStatus;
+  switch (cf) {
+    case Comfort_OK:
+      comfortStatus = "Comfort_OK";
+      break;
+    case Comfort_TooHot:
+      comfortStatus = "Comfort_TooHot";
+      break;
+    case Comfort_TooCold:
+      comfortStatus = "Comfort_TooCold";
+      break;
+    case Comfort_TooDry:
+      comfortStatus = "Comfort_TooDry";
+      break;
+    case Comfort_TooHumid:
+      comfortStatus = "Comfort_TooHumid";
+      break;
+    case Comfort_HotAndHumid:
+      comfortStatus = "Comfort_HotAndHumid";
+      break;
+    case Comfort_HotAndDry:
+      comfortStatus = "Comfort_HotAndDry";
+      break;
+    case Comfort_ColdAndHumid:
+      comfortStatus = "Comfort_ColdAndHumid";
+      break;
+    case Comfort_ColdAndDry:
+      comfortStatus = "Comfort_ColdAndDry";
+      break;
+    default:
+      comfortStatus = "Unknown:";
+      break;
+  };
+
+  Log.trace("temperature: %s\n", String(newValues.temperature).c_str());
+  Log.trace("humidity: %s\n", String(newValues.humidity).c_str());
+  Log.trace("heat index: %s\n", String(heatIndex).c_str());
+  Log.trace("dew point: %s\n", String(dewPoint).c_str());
+  Log.trace("confort status: %s\n", comfortStatus.c_str());
+
+  return true;
+}
+
 
 /* ========================================================================= 
    Public functions 
@@ -30,8 +94,13 @@ void sunriseStateLoop()
     Log.trace("initializing button interrupt\n");
     pinMode(BUTTON_INTERRUPT_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(BUTTON_INTERRUPT_PIN), buttonInterrupt, CHANGE);
+
+    Log.trace("initializing temperature sensor\n");
+    dht.setup(DHT_PIN, DHTesp::DHT11);
+
     globalStatus.goToSunrise = false;
   }
+  logTemperature();
 }
 
 // sunriseStateButtonPress returns true if any button is pressed during sunrise
